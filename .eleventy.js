1b22f6b227edc6d626170f58e36824aadad746bf
Anith Vishwanath <11884278+anithvishwanath@users.noreply.github.com>
2023-12-30 11:25:57 +0530

    Package and CSS Changes.

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,7 @@
 const { DateTime } = require('luxon');
 const { chain, toPairs, value, _ } = require('lodash');
+const markdownIt = require("markdown-it");
+const markdownItAttrs = require("markdown-it-attrs");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addFilter("readableDate", (dateObj) => {
@@ -24,6 +26,16 @@ module.exports = function (eleventyConfig) {
       .reverse()
       .value();
   });
+
+  const mdOptions = {
+    html: true,
+    breaks: true,
+    linkify: true,
+  };
+
+  const markdownLib = markdownIt(mdOptions).use(markdownItAttrs).disable("code");
+
+  eleventyConfig.setLibrary("md", markdownLib);
 
   eleventyConfig.addPassthroughCopy("src/assets");
   eleventyConfig.addPassthroughCopy("src/css");
